ae527e0ccc5450fd02191a05ee6baf9363cb0629
Fulton Garcia <fultongarcia@gmail.com>
2016-09-02 21:48:02 -0400

    updates

--- jScript.js
+++ jScript.js
@@ -6,15 +6,20 @@ $(document).ready(function(){
   });
 
   if (annyang) {
-  // Let's define our first command. First the text we expect, and then the function it should call
-  var commands = {
-    'jazz help': function() {
+    function help() {
+        console.log("help");
       $("#midText").text("Jas Open: Youtube");
       $("#middleDiv").fadeIn("slow","linear");
-    },
-    'Jas open video': function(){
-      window.open("https://www.youtube.com/");
     }
+
+    function video(){
+      console.log("video");
+        window.open("https://www.youtube.com/");
+    }
+  // Let's define our first command. First the text we expect, and then the function it should call
+  var commands = {
+    'Jazz help':{'regexp': /^(jazz|jaz|yazz|yes|chaz|has|jaws|Jazz) (help|halp|hell)$/, 'callback': help()},
+    'Jas open video': {'regexp': /^(jazz|jaz|yazz|yes|chaz|has|jaws|Jazz) (open) (video)$/, 'callback': video()}
   };
 
   // Add our commands to annyang
