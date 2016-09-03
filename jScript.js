f14de2a860d5de5d552728e8e922bf9ece59eb63
Fulton Garcia <fultongarcia@gmail.com>
2016-09-02 21:51:40 -0400

    messing around

--- jScript.js
+++ jScript.js
@@ -5,30 +5,6 @@ $(document).ready(function(){
     date();
   });
 
-  if (annyang) {
-    function help() {
-        console.log("help");
-      $("#midText").text("Jas Open: Youtube");
-      $("#middleDiv").fadeIn("slow","linear");
-    }
-
-    function video(){
-      console.log("video");
-        window.open("https://www.youtube.com/");
-    }
-  // Let's define our first command. First the text we expect, and then the function it should call
-  var commands = {
-    'Jazz help':{'regexp': /^(jazz|jaz|yazz|yes|chaz|has|jaws|Jazz) (help|halp|hell)$/, 'callback': help()},
-    'Jas open video': {'regexp': /^(jazz|jaz|yazz|yes|chaz|has|jaws|Jazz) (open) (video)$/, 'callback': video()}
-  };
-
-  // Add our commands to annyang
-  annyang.addCommands(commands);
-
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
-  annyang.start();
-}
-
 });
 
 function date(){
